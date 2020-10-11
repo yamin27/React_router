893b5f24567b933992c6be0f18a8933248231c20
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-11 13:18:21 +0600

    React Router Basic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Route, Link, BrowserRouter as Router} from '../node_modules/react-router-dom';
+import About from "./Pages/About";
+import Portfolio from "./Pages/Portfolio";
+import Contact from "./Pages/Contact";
+
+
+const myRouter = (
+
+    <Router>
+
+        <div>
+            <ul>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contact">Contact</Link>
+            </ul>
+        </div>
+
+        <Route exact path="/" component={App}/>
+        <Route path="/about" component={About}/>
+        <Route path="/portfolio" component={Portfolio}/>
+        <Route path="/contact" component={Contact}/>
+
+    </Router>
+
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  myRouter,
   document.getElementById('root')
 );
 
